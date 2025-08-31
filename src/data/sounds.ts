3f20cae7f3952d7735b919f7e3b9b1626a616e1e
Milind Mishra <milind.mishra4@gmail.com>
2025-08-31 22:17:02 +0530

    feat: add sounds

--- src/data/sounds.ts
+++ src/data/sounds.ts
@@ -5,12 +5,14 @@ export type Sound = {
 }
 
 export const SOUNDS: Sound[] = [
-  { id: "vine-boom", name: "Vine Boom", url: "/sounds/trex.mp3" },
-  { id: "bruh", name: "Bruh", url: "/sounds/trex.mp3" },
-  { id: "airhorn", name: "Airhorn", url: "/sounds/trex.mp3" },
-  { id: "metal-pipe", name: "Metal Pipe", url: "/sounds/trex.mp3" },
-  { id: "sus", name: "Sus", url: "/sounds/trex.mp3" },
-  { id: "goofy-ahh", name: "Goofy Ahh", url: "/sounds/trex.mp3" },
-  { id: "skibidi", name: "Skibidi", url: "/sounds/trex.mp3" },
-  { id: "rizz", name: "Rizz", url: "/sounds/trex.mp3" },
+  { id: "and-his-name-is-john-cena", name: "And His Name Is John Cena", url: "/sounds/and-his-name-is-john-cena.mp3" },
+  { id: "awkward-cricket-silence", name: "Awkward Cricket Silence", url: "/sounds/awkward-cricket-silence.mp3" },
+  { id: "bruh", name: "Bruh", url: "/sounds/bruh.mp3" },
+  { id: "fart", name: "Fart", url: "/sounds/fart.mp3" },
+  { id: "fbi-open-up", name: "FBI Open Up", url: "/sounds/fbi-open-up.mp3" },
+  { id: "man-screaming", name: "Man Screaming", url: "/sounds/man-screaming.mp3" },
+  { id: "rizz", name: "Rizz", url: "/sounds/rizz.mp3" },
+  { id: "vine-boom", name: "Vine Boom", url: "/sounds/vine-boom.mp3" },
+  { id: "wet-fart", name: "Wet Fart", url: "/sounds/wet-fart.mp3" },
+  { id: "windows-xp-shutdown", name: "Windows XP Shutdown", url: "/sounds/windows-xp-shutdown.mp3" },
 ]
