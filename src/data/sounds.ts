5aa061ee48c4e6b0d7a8a6a3a7000b581a220e80
Milind Mishra <milind.mishra4@gmail.com>
2025-08-31 23:49:24 +0530

    feat: updated sound tracks and data src

--- src/data/sounds.ts
+++ src/data/sounds.ts
@@ -1,18 +1,57 @@
 export type Sound = {
-  id: string
-  name: string
-  url: string
-}
+  id: string;
+  name: string;
+  url: string;
+};
 
 export const SOUNDS: Sound[] = [
-  { id: "and-his-name-is-john-cena", name: "And His Name Is John Cena", url: "/sounds/and-his-name-is-john-cena.mp3" },
-  { id: "awkward-cricket-silence", name: "Awkward Cricket Silence", url: "/sounds/awkward-cricket-silence.mp3" },
+  { id: "7-crore-kbc", name: "7 Crore KBC", url: "/sounds/7-crore-kbc.mp3" },
+  {
+    id: "ab-dekh-tu-puneet-superstar",
+    name: "Ab Dekh Tu Puneet Superstar",
+    url: "/sounds/ab-dekh-tu-puneet-superstar.mp3",
+  },
+  {
+    id: "abhi-maza-aayega-na-bhidu",
+    name: "Abhi Maza Aayega Na Bhidu",
+    url: "/sounds/abhi-maza-aayega-na-bhidu.mp3",
+  },
+  {
+    id: "and-his-name-is-john-cena",
+    name: "And His Name Is John Cena",
+    url: "/sounds/and-his-name-is-john-cena.mp3",
+  },
+  {
+    id: "awkward-cricket-silence",
+    name: "Awkward Cricket Silence",
+    url: "/sounds/awkward-cricket-silence.mp3",
+  },
   { id: "bruh", name: "Bruh", url: "/sounds/bruh.mp3" },
+  {
+    id: "careless-whispers",
+    name: "Careless Whispers",
+    url: "/sounds/careless-whispers.mp3",
+  },
   { id: "fart", name: "Fart", url: "/sounds/fart.mp3" },
   { id: "fbi-open-up", name: "FBI Open Up", url: "/sounds/fbi-open-up.mp3" },
-  { id: "man-screaming", name: "Man Screaming", url: "/sounds/man-screaming.mp3" },
+  { id: "meaw-meaw", name: "Meaw Meaw Sad", url: "/sounds/meaw-meaw-sad.mp3" },
+  {
+    id: "ki-hore-hahaha",
+    name: "Ki Hore Hahaha",
+    url: "/sounds/ki-hore-hahaha.mp3",
+  },
+  {
+    id: "man-screaming",
+    name: "Man Screaming",
+    url: "/sounds/man-screaming.mp3",
+  },
   { id: "rizz", name: "Rizz", url: "/sounds/rizz.mp3" },
+  { id: "sigma-boy", name: "Sigma Boy", url: "/sounds/sigma-boy.mp3" },
   { id: "vine-boom", name: "Vine Boom", url: "/sounds/vine-boom.mp3" },
   { id: "wet-fart", name: "Wet Fart", url: "/sounds/wet-fart.mp3" },
-  { id: "windows-xp-shutdown", name: "Windows XP Shutdown", url: "/sounds/windows-xp-shutdown.mp3" },
-]
+  {
+    id: "windows-xp-shutdown",
+    name: "Windows XP Shutdown",
+    url: "/sounds/windows-xp-shutdown.mp3",
+  },
+];
