9f6e949e44e702b0861d89612cd2d3c2a8a45759
Milind Mishra <milind.mishra4@gmail.com>
2025-08-31 21:52:21 +0530

    feat: nuke analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type React from "react";
 import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
-import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Suspense } from "react";
@@ -47,7 +46,6 @@ export default function RootLayout({
             {children}
           </ThemeProvider>
         </Suspense>
-        <Analytics />
       </body>
     </html>
   );
