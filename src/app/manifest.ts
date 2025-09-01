4e3acd00e6b70922fee2a6414e6888e0f03e1d4e
Milind Mishra <milind.mishra4@gmail.com>
2025-09-02 04:57:26 +0530

    feat: nuke serwist for now and also nuke sounds from public

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,20 +1,25 @@
-import { MetadataRoute } from 'next'
- 
+import { MetadataRoute } from "next";
+
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Rizzboard',
-    short_name: 'Rizzboard',
-    description: 'A soundboard for rizz',
-    start_url: '/',
-    display: 'standalone',
-    background_color: '#fff',
-    theme_color: '#fff',
+    name: "Rizzboard",
+    short_name: "Rizzboard",
+    description: "A soundboard for rizz",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#fff",
     icons: [
       {
-        src: '/favicon.ico',
-        sizes: 'any',
-        type: 'image/x-icon',
+        src: "/icons/192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icons/512.png",
+        sizes: "512x512",
+        type: "image/png",
       },
     ],
-  }
+  };
 }
